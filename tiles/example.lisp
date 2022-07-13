79442ae06b1e39f49528af38cb0ca67828a28bf0
lpaviets <leo.paviet.salomon@gmail.com>
2022-07-13 09:33:00 +0200

    Add a *example-directory* variable to store examples

--- tiles/example.lisp
+++ tiles/example.lisp
@@ -1,9 +1,13 @@
 (in-package #:cl-tikz/tiles)
 
+(defparameter *examples-directory*
+  #P"examples/")
+
 (defun gen-example-tiling (tileset m n)
-  (let ((name (format nil "./examples/test-~A-~Dx~D.tex" (tileset-name tileset) m n))
+  (let ((filename (merge-pathnames (format nil "test-~A-~Dx~D.tex" (tileset-name tileset) m n)
+                                   *examples-directory*))
         (tiling (make-tiling-grid tileset m n)))
-    (with-preamble-to-file (name) ()
+    (with-preamble-to-file (filename) ()
       (with-env (tikzpicture)
         (with-random-crop (0.5 0.5 (- m 0.8) (- n 0.8))
           (draw-tiling (solve-naive tiling :random t)))))))
